5bdf5047754c93d694d32b4623d33b0878652575
Bayo97 <38213770+Bayo97@users.noreply.github.com>
2025-11-30 19:56:34 +0100

    Create filters.py

---
+++ filters.py
@@ -0,0 +1,10 @@
+def is_probable_honeypot(pair):
+    """
+    Placeholder filtr honeypotów.
+    Możesz tu dodać logikę sprawdzania np.:
+    - brak możliwości sprzedaży tokena
+    - niska płynność
+    - bardzo wysoki spread
+    Obecnie zawsze zwraca False (nie jest honeypotem)
+    """
+    return False
